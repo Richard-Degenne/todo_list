e06baebd7e79b2662d164ac84d89402b21454b60
Richard-Degenne <richdeg2@gmail.com>
2017-01-12 11:10:53 +0100

    Implemented `add` subcommand

--- Main.ml
+++ Main.ml
@@ -12,13 +12,28 @@ let list_items =
       |> List.iter ~f:(fun item -> printf "%s\t%s\n" (Item.title item) (Item.contents item))
     )
 
+let add_item =
+  Command.basic
+    ~summary:("Add a new element to the list")
+    Command.Spec.(
+      empty
+      +> anon ("title" %: string)
+      +> anon ("contents" %: string)
+    )
+    (fun title contents () ->
+       Todo.init ()
+       |> Todo.add_item ~item:(Item.create ~title ~contents)
+       |> Todo.save
+    )
+
 let command =
   Command.group
     ~summary:("A simple todo-list manager written in OCaml.")
     ~readme:(fun () -> "\
       TodoList is a software that lets you manage a simple task list : add, list and delete items.\
     ")
-    ["list", list_items]
+    ["list", list_items;
+     "add", add_item]
 
 let () =
   Command.run command ~version:"0.1" ~build_info:"ocamlbuild 4.04.0"
