24d8a88085d5812ab8c99cffe0314ec290e6f8f4
Richard-Degenne <richdeg2@gmail.com>
2017-01-16 22:41:48 +0100

    Tweaked `Todo.init`
    `Todo.init` now looks up the environment for a `HOME` variable. If set,
    it will look for `.todo_list` there instead of the current directory.

--- Todo.ml
+++ Todo.ml
@@ -5,21 +5,29 @@ type t = {
   mutable items: Item.t list
 }
 
+let last_char s =
+  String.get s (String.length s - 1)
+
 let init ?(filename=".todo_list") () =
+  let realpath = match Sys.getenv("HOME") with
+    | Some str -> str ^ (if (last_char str)='/' then "" else "/" ) ^ filename
+    | None -> printf "$HOME is not set. Falling back on current directory.\n"; filename
+  in
   try
-    In_channel.with_file filename ~f:(fun in_c ->
+    In_channel.with_file realpath ~f:(fun in_c ->
       let items = In_channel.fold_lines in_c ~init:[] ~f:(fun acc str -> 
       match String.split str ~on:':' with
       | [title; contents] -> (Item.create ~title ~contents)::acc
-      | _ -> acc (* Ignore malformed lines *)
+      | _ -> printf "Warning: Malformed line \"%s\"\n" str; acc (* Ignore malformed lines *)
       )
       in
-      {filename; items}
+      {filename=realpath; items}
     )
   with Sys_error e -> begin
     (* If the file doesn't exist, create it *)
-    Out_channel.(close (create filename));
-    {filename; items=[]}
+    printf "'%s' doesn't exist. Touching it right now.\n" realpath;
+    Out_channel.(close (create realpath));
+    {filename = realpath; items=[]}
   end
 
 let save t =
